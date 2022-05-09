ccfea3f9068081e76ef5a6444fad8fe91bfff2b6
Andrey Kalashnikov <Kaallaash@gmail.com>
2022-05-09 15:12:09 +0300

    Add TelegramBotHelpers

---
+++ TelegramBotHelpers/Button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBotHelpers
+{
+    public class Button
+    {
+        public static IReplyMarkup GetButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>{ new KeyboardButton { Text = "/my-id"},
+                        new KeyboardButton { Text = "/reg-date" } },
+                    new List<KeyboardButton>{ new KeyboardButton{ Text = "/date"},
+                        new KeyboardButton { Text = "/time" },
+                        new KeyboardButton { Text = "/day" } }
+                }
+            };
+        }
+    }
+}
---
+++ TelegramBotHelpers/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace TelegramBotHelpers
+{
+    internal class Program
+    {
+        static void Main(string[] args) { }
+    }
+}
